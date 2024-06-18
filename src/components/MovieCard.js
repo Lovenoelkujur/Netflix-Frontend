eaa62433695c735dc5115b756a0beecd387f0c24
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-18 22:18:12 +0530

    Search movie function done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { TMDB_IMG_URL } from '../utils/Constant';
 
 const MovieCard = ({posterPath}) => {
+
+    if(posterPath === null) return null;
+    
   return (
     <div className='w-48 pr-2'>
         <img src={`${TMDB_IMG_URL}/${posterPath}`} alt='movie-banner'/>
--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -13,6 +13,8 @@ export const Popular_Movies = "https://api.themoviedb.org/3/movie/popular";
 export const Top_Rated_Movies = "https://api.themoviedb.org/3/movie/top_rated";
 export const Upcomming_Movies = "https://api.themoviedb.org/3/movie/upcoming";
 
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
+
 export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 export default API_END_POINT;
